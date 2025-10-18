31d6f1711122c8d29d7de0b72ac548c21a6d0333
Bruno Nascimento <blnascimento240298@gmail.com>
2025-10-17 21:01:12 -0300

    Alterando o provedor do banco

--- backend/server.js
+++ backend/server.js
@@ -377,7 +377,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_DATABASE
 });
 
 cron.schedule('0 1 * * *', async () => {
